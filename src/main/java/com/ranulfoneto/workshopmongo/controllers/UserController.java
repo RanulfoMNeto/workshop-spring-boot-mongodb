dcaa84920c28a421dda5f564fc8e09523545d6d7
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-31 11:28:40 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/ranulfoneto/workshopmongo/controllers/UserController.java
+++ src/main/java/com/ranulfoneto/workshopmongo/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.ranulfoneto.workshopmongo.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ranulfoneto.workshopmongo.dtos.UserDTO;
 import com.ranulfoneto.workshopmongo.models.User;
 import com.ranulfoneto.workshopmongo.services.UserService;
 
@@ -21,9 +23,10 @@ public class UserController {
     }
     
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x.getId(), x.getName(), x.getEmail())).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
---
+++ src/main/java/com/ranulfoneto/workshopmongo/dtos/UserDTO.java
@@ -0,0 +1,5 @@
+package com.ranulfoneto.workshopmongo.dtos;
+
+public record UserDTO(String id, String name, String email) {
+
+}
