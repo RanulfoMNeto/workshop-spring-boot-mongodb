f1341608473d24f5d0fd772bde29a32c9eed15d2
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-08-01 10:13:18 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/ranulfoneto/workshopmongo/controllers/UserController.java
+++ src/main/java/com/ranulfoneto/workshopmongo/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,12 @@ public class UserController {
         user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
